047aae18d1e7f6eee7a3b3a09a534db60dc53f1e
BIL-LAB2PC00\Ogrenci <seyydinburhanedin.mtal38@gmail.com>
2023-06-06 12:13:13 +0300

    no message

---
+++ js/app5.js
@@ -0,0 +1,7 @@
+function ekle(){
+    var x = document.getElementById("num");
+    var y = document.getElementById("ad");
+    var z = document.getElementById("sy");
+    var sonuc = x + y + z;
+    alert(sonuc)
+}
